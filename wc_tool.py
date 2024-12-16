99281371896120f28c436fe4dfe125df17615ab4
aashishraghav627@gmail.com <aashishraghav627@gmail.com>
2024-12-16 23:27:43 +0530

    Unix wc_tool: Read from stdin
    Update to Read from stdin if no filename is given
    Update Executable binary

--- wc_tool.py
+++ wc_tool.py
@@ -1,14 +1,14 @@
 import argparse
 import os
+import sys
 
-def count_lines_words_characters_bytes(filename):
-    with open(filename,'r',encoding='utf-8') as file:
-        lines_list = file.readlines()
-        lines = len(lines_list)
-        words = sum(len(line.split()) for line in lines_list)
-        characters = sum((len(line)+1) for line in lines_list) 
+def count_lines_words_characters_bytes(file_content):
+    lines_list = file_content.splitlines()
+    lines = len(lines_list)
+    words = sum(len(line.split()) for line in lines_list)
+    characters = sum((len(line)+1) for line in lines_list) 
+    bytes = len(file_content.encode('utf-8'))
 
-    bytes = os.stat(filename).st_size
 
     return (lines,words,characters,bytes)
 
@@ -16,7 +16,11 @@ def count_lines_words_characters_bytes(filename):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(prog="WC Tool",description="Unix version of WC tool")
-    parser.add_argument("filename")
+    parser.add_argument("filename",
+                        type=str,
+                        nargs='?',
+                        help="Path to the file to process. Reads from stdin if not provided.")
+    
     parser.add_argument('-c','--bytes',action='store_true',help="print the byte counts")
     parser.add_argument('-m','--chars',action='store_true',help="print the character counts")
     parser.add_argument('-w','--words',action='store_true',help="print the word counts")
@@ -25,8 +29,16 @@ if __name__ == '__main__':
 
     filename = f"{args.filename}"
     try:
-        lines,words,characters,bytes = count_lines_words_characters_bytes(filename)
+        if (args.filename):
+            with open(args.file, 'r', encoding='utf-8') as file_handle:
+                content = file_handle.read()
 
+
+        else:
+            print("Reading from standard input. Press Ctrl+D (Unix) or Ctrl+Z (Windows) to finish.")
+            content = sys.stdin.read()
+
+        lines,words,characters,bytes = count_lines_words_characters_bytes(content)
         output_str = ""
         if (args.lines): 
             output_str += f"{lines} "
@@ -43,8 +55,12 @@ if __name__ == '__main__':
         if (len(output_str) == 0):
             output_str += f"{lines} {words} {bytes} "
         
-        output_str += filename
+        if (args.filename):
+            output_str += filename
+        else:
+            output_str += "stdin"
         print(output_str)
+
     except FileNotFoundError:
         print(f"File {filename} not found.")
     except Exception as e:
